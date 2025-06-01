7f2d8f48c25495e3ab7e4a7e298fe77a28954ade
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-06-01 19:14:10 +0530

    initial setup of go backend

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,5 @@
 module github.com/tashifkhan/QuipQuest/backend
 
 go 1.24.3
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Hi from the backend!")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/api/check", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello from the backend!")
+		fmt.Println("Received a request on /api/check")
+	}).Methods("GET")
+
+	http.Handle("/", r)
+	fmt.Println("Server is running on port 6990")
+	http.ListenAndServe(":6990", nil)
 }
