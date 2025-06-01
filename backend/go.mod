7e2337fd5c22685083660cb4b3f1bca4c1c6bea1
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-06-02 03:03:14 +0530

    loading env varibles

--- backend/go.mod
+++ backend/go.mod
@@ -3,3 +3,5 @@ module github.com/tashifkhan/QuipQuest/backend
 go 1.24.3
 
 require github.com/gorilla/mux v1.8.1
+
+require github.com/joho/godotenv v1.5.1
--- backend/main.go
+++ backend/main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"github.com/tashifkhan/QuipQuest/backend/db/users"
 	"github.com/tashifkhan/QuipQuest/backend/router"
 )
 
 func main() {
 	fmt.Println("Hi from the backend!")
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	databaseURL := os.Getenv("DATABASE_URL")
+	println("Database URL:", databaseURL)
 
 	r := mux.NewRouter().
 		PathPrefix("/api/").
